da5cb6a10a3c001e754c4170d9f5ac11999336f5
Bahoooo <154087517+Bahoooo@users.noreply.github.com>
2023-12-18 00:32:45 +0400

    Update products.js

--- products.js
+++ products.js
@@ -57,5 +57,17 @@ const productList = [
       price: '45 AZN',
       description: 'A thoughtful gift for any occasion.',
       image: 'image1.jpg',
-      link: 'https://umico.az/product/339752-qadinlar-uchun-etir-suyu-lattafa-perfumes-yara-100-ml'},      
+      link: 'https://umico.az/product/339752-qadinlar-uchun-etir-suyu-lattafa-perfumes-yara-100-ml'},
+
+    { name: 'Soussurit',
+      recipient: 'Colleague',
+      age: '20-25',
+      gender: 'Male',
+      occasion: 'Secret Santa',
+      interests: ['Arts','Sports','Hiking','Science','Other'],
+      budget: '0-30',
+      price: '9.99 AZN',
+      description: 'A thoughtful gift for any occasion.',
+      image: 'santa1.jpg',
+      link: 'https://umico.az/product/402248-yeni-il-elfi-jysk-soussurit'},
 ];
